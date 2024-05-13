0e7ac23e9f1047f53f4012e286b5cb0a7eaffc61
jw.lee <jwlee36963@gmail.com>
2024-05-13 11:10:27 +0800

    feat: 完善 setGitEnv.sh

--- setGitEnv.sh
+++ setGitEnv.sh
@@ -4,3 +4,4 @@ git config --local user.name "jw.lee"
 git config --local user.email "jwlee36963@gmail.com"
 git config --local core.quotePath false
 git config --local core.editor "vim"
+git config --local core.autocrlf false
