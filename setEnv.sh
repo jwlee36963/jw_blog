ea5a5876b72c4590084b2035a2b95febb3e378b1
jw.lee <jwlee36963@gmail.com>
2024-05-15 14:10:14 +0800

    feat: 改进博客环境设置脚本

---
+++ setEnv.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+git config --local user.name "jw.lee"
+git config --local user.email "jwlee36963@gmail.com"
+git config --local core.quotePath false
+git config --local core.editor "vim"
+git config --local core.autocrlf false
+
+# 保持blog/source_posts为最新
+# 检查当前路径是否有blog文件夹,有则切换到blog目录
+if [ -d "blog" ]; then
+  cd blog
+else
+  echo "当前路径下没有blog文件夹,无法切换."
+  exit 1
+fi
+
+# 切换到source目录
+cd source
+
+# 删除_posts目录
+if [ -d "_posts" ]; then
+  rm -rf "_posts"
+else
+  echo "source目录下没有_posts目录,无需删除."
+fi
+
+# 返回上一级目录
+cd ../..
+
+# 将_posts目录复制到blog/source目录
+if [ -d "_posts" ]; then
+  cp -rfp "_posts" "blog/source"
+else
+  echo "当前路径下没有_posts目录,无法复制."
+fi
+
+echo "blog/source/_posts 已刷新"
--- setGitEnv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --local user.name "jw.lee"
-git config --local user.email "jwlee36963@gmail.com"
-git config --local core.quotePath false
-git config --local core.editor "vim"
-git config --local core.autocrlf false
